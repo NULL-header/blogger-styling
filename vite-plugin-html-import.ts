1a058b94e0e0d563d4cf19828376593077c0927f
NULL-header <poaoq.poaoq@gmail.com>
2024-09-07 23:50:18 +0900

    update dev env

--- vite-plugin-html-import.ts
+++ vite-plugin-html-import.ts
@@ -8,12 +8,19 @@ export default function HtmlRawImport() {
     transform(src, id) {
       if (!IMPORT_PATH.test(id)) return;
       return {
-        code: HTMLMinifier.minify(src.replaceAll("\\r", ""), {
-          removeAttributeQuotes: true,
-          collapseWhitespace: true,
-          trimCustomFragments: true,
-          minifyCSS: true,
-        }),
+        code: HTMLMinifier.minify(
+          src
+            .replaceAll("\\r", "")
+            .replaceAll("\\n", "")
+            .replaceAll('\\"', '"'),
+          {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            trimCustomFragments: true,
+            minifyCSS: true,
+            quoteCharacter: "'",
+          }
+        ),
       };
     },
   };
