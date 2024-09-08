5813e2c63e12fa77c02e2fe771bdbea321cd811d
NULL-header <poaoq.poaoq@gmail.com>
2024-09-08 14:54:55 +0900

    add pre-styled animation-details

---
+++ src/pre-styled/animated-details/index.ts
@@ -0,0 +1,27 @@
+import HTMLString from "./index.html?raw";
+import styles from "./main.css?inline";
+import "@/pure_animated-details.js";
+
+class AnimatedDetails extends HTMLElement {
+  private summary: HTMLElement;
+  constructor() {
+    super();
+    const template = document.createElement("template");
+    template.innerHTML = HTMLString;
+    const styleEl = document.createElement("style");
+    styleEl.innerHTML = styles;
+    template.content.appendChild(styleEl);
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(template.content.cloneNode(true));
+    const summary = shadow.querySelector("*[data-title]") as HTMLElement;
+    this.summary = summary;
+  }
+
+  connectedCallback() {
+    const title = this.dataset.title;
+    if (title == null) return;
+    this.summary.innerHTML = title;
+  }
+}
+
+customElements.define("pre-styled-animated-details", AnimatedDetails);
